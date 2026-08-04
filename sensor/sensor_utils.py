5b6110ff104ab213ec544a7f13eb4c48bbdd64ef
Callum Gundlach <gundlachcallum@gmail.com>
2026-08-03 21:49:12 -0700

    Remove setup.py in favor of poetry

--- sensor/sensor_utils.py
+++ sensor/sensor_utils.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import asyncio
 import threading
 import time
--- train_single_sub/utils/utils.py
+++ train_single_sub/utils/utils.py
@@ -4,9 +4,8 @@ import pickle
 
 def load_pkl_data(file_path):
     try:
-        f = open(file_path, 'rb')
-        raw_data = pickle.load(f)
-        f.close()
+        with open(file_path, "rb") as f:
+            raw_data = pickle.load(f)
         return raw_data
     except Exception as err:
         print(file_path)
@@ -14,9 +13,9 @@ def load_pkl_data(file_path):
 
 
 def join_path(*sub_path):
-    path = ''
+    path = ""
     for p in sub_path:
         path = os.path.join(path, p)
-    if (path.startswith('../')):
-        path = path.replace('\\', '/')
-    return path.replace('\r', '\\r').replace('\n', '\\n')
+    if path.startswith("../"):
+        path = path.replace("\\", "/")
+    return path.replace("\r", "\\r").replace("\n", "\\n")
